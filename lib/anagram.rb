a5843612bfb2d4361f80dc537857019b5ccab652
Conor Hamilton <conhamil19@gmail.com>
2018-05-22 12:35:53 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
-# Your code goes here!
+class Anagram 
+  attr_accessor :word 
+  
+  def initialization(word)
+    @word = word
+  end
+  
+  def match(array)
+    array.select do |words|
+      @word == words
+    end
+  end 
+end
